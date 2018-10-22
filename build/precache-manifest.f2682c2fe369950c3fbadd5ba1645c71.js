022f2e7adfc979574199440f0f8d97381e85ff37
Franciszek Andruszkiewicz <fandruszkiewicz@gmail.com>
2018-10-22 19:27:04 +0200

    dodano build

---
+++ build/precache-manifest.f2682c2fe369950c3fbadd5ba1645c71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2af6efb83eb1412a7c73",
+    "url": "/static/js/main.2af6efb8.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "2af6efb83eb1412a7c73",
+    "url": "/static/css/main.67110ca1.chunk.css"
+  },
+  {
+    "revision": "022770c6f1f1633e7135a4099f39b77c",
+    "url": "/index.html"
+  }
+];
